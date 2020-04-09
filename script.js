3ea0fe7bd220b055c8876453cdb27c36fbc040cb
kmontalvom <kmontalvom@ucic.pe>
2020-04-09 14:05:11 -0500

    upadte try

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("http://201.209.104.33:3000");
+const socket = io("https://kathmontalvo.github.io:3000");
 const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
